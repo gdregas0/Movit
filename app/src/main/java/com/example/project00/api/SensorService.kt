63e35ec95ce75b260b32370d76920ba8179d03e1
eyedi <gdregas18@gmail.com>
2019-10-13 23:52:35 +0900

    [Commit] 1. add notification image 2. foreground service making, but need to debugging more.. 3. android gradle tool version upgrade

--- app/src/main/java/com/example/project00/api/SensorService.kt
+++ app/src/main/java/com/example/project00/api/SensorService.kt
@@ -1,15 +1,25 @@
 package com.example.project00.api
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
+
 import android.os.IBinder
+import android.os.Parcelable
 import android.os.PowerManager
+
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import com.example.project00.ui.gallery.GalleryViewModel
 import java.lang.UnsupportedOperationException
@@ -23,6 +33,52 @@ class SensorService : Service(), SensorEventListener {
 
     private val sensorMNG : SensorManager by lazy {
         getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    companion object {
+        private const val TAG = "TestService"
+
+        const val EXTRA_NOTIFICATION_REQUEST_CODE = "EXTRA_NOTIFICATION_REQUEST_CODE"
+        const val EXTRA_NOTIFICATION = "EXTRA_NOTIFICATION"
+
+        fun showNotification(context: Context, requestCode: Int, notification: Notification): Boolean {
+            val intent = Intent(context, SensorService::class.java)
+            intent.putExtra(EXTRA_NOTIFICATION_REQUEST_CODE, requestCode)
+            intent.putExtra(EXTRA_NOTIFICATION, notification)
+            return startService(context, intent)
+        }
+
+        fun stop(context: Context) {
+            val intent = Intent(context, SensorService::class.java)
+            context.stopService(intent)
+        }
+
+        private fun startService(context: Context, intent: Intent): Boolean {
+            // Similar to ContextCompat.startForegroundService(context, intent)
+            val componentName: ComponentName? = if (Build.VERSION.SDK_INT >= 26) {
+                context.startForegroundService(intent)
+            } else {
+                context.startService(intent)
+            }
+            return componentName != null
+        }
+
+        @RequiresApi(api = 26)
+        fun createNotificationChannel(context: Context,
+                                      id: String, name: String, importance: Int,
+                                      description: String) {
+            val channel = NotificationChannel(id, name, importance)
+            channel.description = description
+            createNotificationChannel(context, channel)
+        }
+
+        @Suppress("MemberVisibilityCanBePrivate")
+        @RequiresApi(api = 26)
+        fun createNotificationChannel(context: Context,
+                                      channel: NotificationChannel) {
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -46,11 +102,35 @@ class SensorService : Service(), SensorEventListener {
             SensorManager.SENSOR_DELAY_NORMAL
         )
 
+        try {
+            Log.d(TAG, "+onStartCommand(...)")
+            if (intent != null) {
+                val extras = intent.extras
+                if (extras != null) {
+                    if (extras.containsKey(EXTRA_NOTIFICATION)) {
+                        val notification = extras.getParcelable<Parcelable>(EXTRA_NOTIFICATION)
+                        if (notification is Notification) {
+                            if (extras.containsKey(EXTRA_NOTIFICATION_REQUEST_CODE)) {
+                                val requestCode = extras.getInt(EXTRA_NOTIFICATION_REQUEST_CODE)
+                                startForeground(requestCode, notification)
+                            }
+                        }
+                    }
+                }
+            }
+            return START_NOT_STICKY
+        } finally {
+            Log.d(TAG, "-onStartCommand(...)")
+        }
+
         return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         Log.i("Debug", "destroyed")
+        //PbLog.s(TAG, PbStringUtils.separateCamelCaseWords("onDestroy"));
+        super.onDestroy()
+        stopForeground(true)
         super.onDestroy()
     }
 
--- app/src/main/java/com/example/project00/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/project00/ui/gallery/GalleryFragment.kt
@@ -1,33 +1,53 @@
 package com.example.project00.ui.gallery
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.project00.R
 import com.example.project00.api.SensorService
 
-class GalleryFragment : Fragment(), SensorEventListener {
+class GalleryFragment : Fragment() {
 
     private lateinit var galleryViewModel: GalleryViewModel
 
+    companion object {
+        private const val TAG = "MainApplication"
+
+        fun getMainApplication(context: Context): GalleryFragment {
+            return context.applicationContext as GalleryFragment
+        }
+
+        private const val NOTIFICATION_REQUEST_CODE = 100
+        private const val NOTIFICATION_CHANNEL_ID = "notification_channel_id"
+    }
+
+    private lateinit var notification: Notification
+
+    var isNotificationShowing: Boolean = false
+        private set
+
+    @SuppressLint("InlinedApi")
+    @RequiresApi(api = 26)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        /* View model object created
-        * */
+        /* View model object created */
         galleryViewModel =
             ViewModelProviders.of(this).get(GalleryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_gallery, container, false)
@@ -36,6 +56,20 @@ class GalleryFragment : Fragment(), SensorEventListener {
         galleryViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            val appName = getString(R.string.app_name)
+            val channelName = "$appName channel name"
+            val channelImportance = NotificationManager.IMPORTANCE_LOW
+            val channelDescription = "$appName channel description"
+
+            SensorService.createNotificationChannel(super.getContext() as Context,
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                channelImportance,
+                channelDescription)
+        }
+        notification = createOngoingNotification(NOTIFICATION_REQUEST_CODE, R.drawable.ic_notification, "Content Text")
 
         return root
     }
@@ -46,10 +80,32 @@ class GalleryFragment : Fragment(), SensorEventListener {
         val AccelService = context?.startService(Intent(context, SensorService::class.java))
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        Log.d("Debug", "Accuracy changed ?")
+    @RequiresApi(api = 26)
+    private fun createOngoingNotification(requestCode: Int, icon: Int, text: String): Notification {
+
+        val context: Context = super.getContext() as Context
+
+        val contentIntent = Intent(context, GalleryFragment::class.java)
+            .setAction(Intent.ACTION_MAIN)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        val contentPendingIntent = PendingIntent.getActivity(context, requestCode, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        return Notification.Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setOngoing(true)
+            .setSmallIcon(icon)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText(text)
+            .setContentIntent(contentPendingIntent)
+            .build()
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
+    fun showNotification(show: Boolean) {
+        if (show) {
+            SensorService.showNotification(super.getContext() as Context, NOTIFICATION_REQUEST_CODE, notification)
+            isNotificationShowing = true
+        } else {
+            isNotificationShowing = false
+            SensorService.stop(super.getContext() as Context)
+        }
     }
 }
